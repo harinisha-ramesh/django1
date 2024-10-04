5ace9a82ac74ecfcbba33f27ddc3a7d466d9e780
harinisha-ramesh <harinisharamesh355@gmail.com>
2024-10-04 16:15:36 +0530

    new update

--- new_practice/my_application/views.py
+++ new_practice/my_application/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def homepage(request):
-    return render(request, 'index.html')
+    data = {
+        "name" : "Nick",
+        "age" : 25
+    }
+    return render(request, 'index.html',data)
