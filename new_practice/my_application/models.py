1d4d38e61e5e36a3db51413086958f770f91cc3a
harinisha-ramesh <harinisharamesh355@gmail.com>
2024-10-08 18:30:24 +0530

    modify all files

--- new_practice/my_application/models.py
+++ new_practice/my_application/models.py
@@ -6,3 +6,7 @@ class Product(models.Model):
     price = models.FloatField(default=0)
     gst = models.IntegerField(default=0)
     food_product = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.product_name + " " + self.product_code
+       
